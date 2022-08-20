498da0c650bca51567bc86fa09a5184d02fae340
Võ Văn Bình <binh.vovan38@gmail.com>
2022-08-20 13:31:04 +0700

    Begin ORG

--- WTCS/Program.cs
+++ WTCS/Program.cs
@@ -17,6 +17,7 @@ namespace WTCS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmLogin());
+
         }
     }
 }
